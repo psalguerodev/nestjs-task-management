58b1de93f6bc5e01f1742e18bd57fc0fbb096e7b
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2019-10-13 19:13:17 -0500

    feat(CD): was added some files to config CD of Travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+set -x
+tar -czf package.tgz dist && \
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
