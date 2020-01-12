45595346d30563185a027bc0285b277f7ba746e1
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2020-01-12 12:34:01 -0500

    feat(travis): Was added sshpass to vps

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker push psalguero/nestjs-task-management:latest
 echo 'Docker ::: Push docker image successfully!'
+
+echo 'Connect with VPS Digital Ocean'
+sshpass -p 'psalguero' ssh psalguero@142.93.89.210
+
+docker ps
